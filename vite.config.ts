8507d790d97a70ca8fd9f3f7fcf14f9ce76c3392
nitishnk <nitishgidde44@gmail.com>
2025-05-26 22:05:31 +0530

    second

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nitishnk.github.io/',
+  base: '/NewPortfolio',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
